f15bf407e9bc95560a479bd5ae453ea2da3613fc
Mizore-Nekoyanagi <37854026+SleetCat123@users.noreply.github.com>
2023-12-19 00:20:56 +0900

    - オブジェクト名の変更時に参照が維持されるようにした（ウィンドウを開いているときのみ） - 内部処理の最適化

---
+++ Packages/com.mizore-nekoyanagi.utils.bookmark-and-history/Editor/ObjectWithPath.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace MizoreNekoyanagi.PublishUtil.BookmarkAndHistory {
+    public class ObjectWithPath {
+        Object obj;
+        string path;
+        public Object Object {
+            get {
+                if ( obj == null ) {
+                    obj = AssetDatabase.LoadAssetAtPath<Object>( path );
+                }
+                return obj;
+            }
+        }
+        public string Path {
+            get {
+                if ( obj != null ) {
+                    path = AssetDatabase.GetAssetPath( obj );
+                }
+                return path;
+            }
+        }
+        public ObjectWithPath( Object obj ) {
+            this.obj = obj;
+        }
+        public ObjectWithPath( string path ) {
+            this.path = path;
+        }
+
+        public static implicit operator ObjectWithPath( Object obj ) {
+            return new ObjectWithPath( obj );
+        }
+    }
+}
--- Packages/com.mizore-nekoyanagi.utils.bookmark-and-history/Editor/SelectionHistoryData.cs
+++ Packages/com.mizore-nekoyanagi.utils.bookmark-and-history/Editor/SelectionHistoryData.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 namespace MizoreNekoyanagi.PublishUtil.BookmarkAndHistory {
     [System.Serializable]
-    public class SelectionHistoryData : IEnumerable<string> {
-        [SerializeField]
-        List<string> history = new  List<string>(CAPACITY);
+    public class SelectionHistoryData : IEnumerable<ObjectWithPath>, ISerializationCallbackReceiver {
         const string PATH_HISTORY = "BookmarkAndHistory/MizoresSelectionHistory.json";
+
+        [System.NonSerialized] List<ObjectWithPath> historyObjects = new  List<ObjectWithPath>(CAPACITY);
+        /// <summary>
+        /// Serialize用
+        /// </summary>
+        [SerializeField] string[] history = new string[0];
+
+        public ObjectWithPath this[int index] {
+            get {
+                return historyObjects[index];
+            }
+        }
+        public int Count {
+            get {
+                return historyObjects.Count;
+            }
+        }
 
         const int CAPACITY = MAX_HISOTRY + 1;
         const int MAX_HISOTRY = 50;
@@ -26,26 +42,56 @@ namespace MizoreNekoyanagi.PublishUtil.BookmarkAndHistory {
                 string json = File.ReadAllText(PATH_HISTORY);
                 Debug.Log( "History Loading: \n" + json );
                 JsonUtility.FromJsonOverwrite( json, this );
-                while ( MAX_HISOTRY < history.Count ) {
-                    history.RemoveAt( 0 );
+                while ( MAX_HISOTRY < historyObjects.Count ) {
+                    historyObjects.RemoveAt( 0 );
                 }
-                history.Capacity = CAPACITY;
+                historyObjects.Capacity = CAPACITY;
             }
+        }
+        public void AddHisotry( Object obj ) {
+            if ( obj == null ) {
+                return;
+            }
+            historyObjects.RemoveAll( v => v.Object == obj );
+            while ( MAX_HISOTRY <= historyObjects.Count ) {
+                historyObjects.RemoveAt( 0 );
+            }
+            historyObjects.Add( obj );
         }
         public void AddHisotry( string path ) {
-            history.RemoveAll( v => v == path );
-            history.Add( path );
-            while ( MAX_HISOTRY < history.Count ) {
-                history.RemoveAt( 0 );
+            if ( string.IsNullOrEmpty( path ) ) {
+                return;
             }
+            historyObjects.RemoveAll( v => v.Path == path );
+            while ( MAX_HISOTRY <= historyObjects.Count ) {
+                historyObjects.RemoveAt( 0 );
+            }
+            historyObjects.Add( new ObjectWithPath( path ) );
+        }
+        public void RemoveHistory( string path ) {
+            historyObjects.RemoveAll( v => v.Path == path );
+        }
+        public void RemoveHistory( Object obj ) {
+            historyObjects.RemoveAll( v => v.Object == obj );
         }
 
-        public IEnumerator<string> GetEnumerator( ) {
-            return history.GetEnumerator( );
+        public IEnumerator<ObjectWithPath> GetEnumerator( ) {
+            return historyObjects.GetEnumerator( );
         }
 
         IEnumerator IEnumerable.GetEnumerator( ) {
-            return history.GetEnumerator( );
+            return historyObjects.GetEnumerator( );
+        }
+
+        public void OnBeforeSerialize( ) {
+            history = historyObjects.ConvertAll( v => v.Path ).ToArray( );
+        }
+
+        public void OnAfterDeserialize( ) {
+            historyObjects.Clear( );
+            foreach ( var path in history ) {
+                historyObjects.Add( new ObjectWithPath( path ) );
+            }
         }
     }
 }
